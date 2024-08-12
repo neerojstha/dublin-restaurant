d33afc320390473c3c3bfdcde9185f6bcc19c55b
neerojstha <nirojstha@hotmail.com>
2024-08-12 12:47:55 +0000

    typo error

--- Restaurantconcierge/settings.py
+++ Restaurantconcierge/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dublin-restaurant-6cd4a822f329.herokuapp.com', "localhost"]
+ALLOWED_HOSTS = ["dublin-restaurant-6cd4a822f329.herokuapp.com", "localhost"]
 
 
 # Application definition
